ec7cd173723cde4b3aa6dd5d4b1bd59de7fa0b4a
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-17 05:36:38 +0530

    added env file support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+require('dotenv').config();
+
+const {
+  GRAPHQL_URI,
+} = process.env;
+
 module.exports = {
+  env: {
+    GRAPHQL_URI,
+  },
   experimental: {
     jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
   }
-}
+};
