4fc771e113b21215784baaa1d1c53f76da9c7050
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-17 05:36:53 +0530

    setup Apollo client with common Page component

---
+++ components/common/page.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import { client } from '@lib/init-apollo';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const Page = ({ title, children }) => {
+	return (
+		<ApolloProvider client={client}>
+			<Head>
+				<title>{title} - JunoPact</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link
+					rel="stylesheet"
+					href="https://cdn.jsdelivr.net/npm/font-proxima-nova@1.0.1/style.css"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://cdn.rawgit.com/mfd/f3d96ec7f0e8f034cc22ea73b3797b59/raw/856f1dbb8d807aabceb80b6d4f94b464df461b3e/gotham.css"
+				/>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+			</Head>
+			<div>
+				{children()}
+			</div>
+		</ApolloProvider>
+	);
+};
+
+export default Page;
